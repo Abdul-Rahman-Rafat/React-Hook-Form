3e13eb41fe08ef38267d6eadac858c0a92b3a5a0
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-07-06 15:13:34 +0300

    add user information form UI "Sign up"

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,182 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="w-1/2 m-auto mt-2.5 text-center bg-blue-200 text-blue-900 p-3 rounded-2xl ">
+      <div className="mx-auto mt-16 max-w-7xl text-center bg-[#101828] text-white p-3 rounded-2xl shadow-lg  sm:mt-20">
         {" "}
-        React Hook Form
+        React Hook Form " Sign up "
       </div>
+      <form className="mx-auto mt-16 max-w-7xl sm:mt-20 bg-[#101828] p-10 rounded-2xl">
+        <div>
+          <h2 className="text-base/7 font-semibold text-white">
+            Personal Information
+          </h2>
+          <p className="mt-1 text-sm/6 text-gray-400">
+            Use a permanent address where you can receive mail.
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block text-sm/6 font-medium text-white"
+              >
+                First name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  type="text"
+                  name="first-name"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="last-name"
+                className="block text-sm/6 font-medium text-white"
+              >
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="last-name"
+                  type="text"
+                  name="last-name"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-white"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="country"
+                className="block text-sm/6 font-medium text-white"
+              >
+                Country
+              </label>
+              <div className="mt-2 grid grid-cols-1">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country-name"
+                  className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white/5 py-1.5 pr-8 pl-3 text-base text-white outline-1 -outline-offset-1 outline-white/10 *:bg-gray-800 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                >
+                  <option>United States</option>
+                  <option>Canada</option>
+                  <option>Mexico</option>
+                </select>
+                <svg
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  data-slot="icon"
+                  aria-hidden="true"
+                  className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-400 sm:size-4"
+                >
+                  <path
+                    d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="street-address"
+                className="block text-sm/6 font-medium text-white"
+              >
+                Street address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="street-address"
+                  type="text"
+                  name="street-address"
+                  autoComplete="street-address"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2 sm:col-start-1">
+              <label
+                htmlFor="city"
+                className="block text-sm/6 font-medium text-white"
+              >
+                City
+              </label>
+              <div className="mt-2">
+                <input
+                  id="city"
+                  type="text"
+                  name="city"
+                  autoComplete="address-level2"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="region"
+                className="block text-sm/6 font-medium text-white"
+              >
+                State / Province
+              </label>
+              <div className="mt-2">
+                <input
+                  id="region"
+                  type="text"
+                  name="region"
+                  autoComplete="address-level1"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="postal-code"
+                className="block text-sm/6 font-medium text-white"
+              >
+                ZIP / Postal code
+              </label>
+              <div className="mt-2">
+                <input
+                  id="postal-code"
+                  type="text"
+                  name="postal-code"
+                  autoComplete="postal-code"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     </>
   );
 }
