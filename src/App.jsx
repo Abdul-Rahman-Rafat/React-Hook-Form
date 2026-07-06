699b07ab406068091b71316cad7cd639a2048c0b
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-07-06 14:53:32 +0300

    install React Hook Form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div className="w-1/2 m-auto mt-2.5 text-center bg-blue-200 text-blue-900 p-3 rounded-2xl ">
+        {" "}
+        React Hook Form
+      </div>
+    </>
+  );
+}
+
+export default App;
